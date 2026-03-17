d1e9645c2873b2df8965aaa9937d6dcb201ab626
kerui-1109 <s1130184@dmhs.kh.edu.tw>
2026-03-17 11:16:15 +0800

    解題流程

---
+++ d064ㄑㄧ奇數查詢.cpp
@@ -0,0 +1,23 @@
+//============================================================================
+// Name        : apcs.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int i;
+	cin >> i;
+	
+	if((1 % 2) == 1) {
+		cout << "Odd" << endl;
+	}else {
+		cout << "even" << endl;
+	}
+	
+ 	return 0;
+}
