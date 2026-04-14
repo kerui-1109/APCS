9bde2f75b5dc3d97b75d5a55ce2debf0fb6e12fd
kerui-1109 <s1130184@dmhs.kh.edu.tw>
2026-04-14 11:32:19 +0800

    解題過程

---
+++ a147.Print it all.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    while (cin >> n){
+    	for (int i = 1; i < n; j++){
+    		if (i % 7 ! = 0){
+    			cout << i <<" ";
+    		}
+    }
+    cout<<"\n";
+	return 0;
+}
